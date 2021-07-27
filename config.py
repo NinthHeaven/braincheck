d854d252a310d51b6c4b0df37020689564445a52
Saul Soto <ssoto1@college.harvard.edu>
2021-07-27 04:18:12 -0400

    more config changes

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 # importing configurations
 import os
+import re
 
 # default configs for local
 class defaultConfig(object):
     DEBUG = False
     SECRET_KEY = 'YK\xa0f\n\x82r\x96\xe4\xb2f\x7f\n\xab\x1d\x9bj\xf4\xf1U\x91\xaa\x04\xb2'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.getenv['DATABASE_URL']
     if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
